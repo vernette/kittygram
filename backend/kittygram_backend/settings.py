9050939e52ff5ff8cb6054a9831f65b2b58dfd16
Skryabin Nikita <nekit2271@gmail.com>
2024-05-08 14:23:21 +0300

    Add domain to ALLOWED_HOSTS

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,9 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kittygram-public.hopto.org'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
