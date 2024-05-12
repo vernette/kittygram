d564db6b9b55b5899c952ed1a338cdeb907b57ee
Skryabin Nikita <nekit2271@gmail.com>
2024-05-12 15:05:05 +0300

    Change debug default value

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv(
     get_random_secret_key()
 )
 
-DEBUG = os.getenv('DJANGO_DEBUG', 'True')
+DEBUG = os.getenv('DJANGO_DEBUG', 'False')
 
 ALLOWED_HOSTS = [
     'kittygram-public.hopto.org'
